b63c3a3510997c6bd6cb4c7c6627c064532c4ead
Devesh Shukla <devshukla291698@gmail.com>
2025-11-19 14:53:13 +0530

    practice code on Main.java

--- Main.java
+++ Main.java
@@ -1,22 +1,47 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         // Input
-        int g = sc.nextInt();
-        int c = sc.nextInt();
-        int l = sc.nextInt();
+        int t = sc.nextInt(); // number of test cases
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt(); // number of toys
+            int[] arr = new int[n];
+            for (int j = 0; j < n; j++) {
+                arr[j] = sc.nextInt();
+            }
+            problem(arr, n);
+        }
+        sc.close();
+        
+    }
 
+    public static void problem(int[] arr, int n) {
         // Logic
-        int arr[] = {g,c,l};
-        Arrays.sort(arr);
+        for(int i=0; i<arr.length; i++) {
+            for(int j=i+1; j<arr.length; j++) {
+                if (arr[i]%2 == 0 && arr[j]%2 != 0 && arr[i]>arr[j]) {
+                    //swap
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                } else if (arr[i]%2 != 0 && arr[j]%2 == 0 && arr[i]>arr[j]){
+                    //swap
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
 
-        if (arr[2] - arr[0] >= 10) {
-            System.out.println("check again");
-        }else
-            System.out.println("final "+arr[1]);
+        // Print output cleanly
+        StringBuilder sb = new StringBuilder();
+        for (int k = 0; k < n; k++) {
+            sb.append(arr[k]);
+            if (k < n - 1) sb.append(" ");
+        }
+        System.out.println(sb.toString());
     }
 }
